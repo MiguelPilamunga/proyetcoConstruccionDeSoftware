f85582153727f4c4217fbd50a6acb871ad3caa04
MiguelPilamunga <luis.pilamunga@epn.edu.ec>
2023-09-09 17:01:30 -0500

    API release1

---
+++ src/test/java/epn/edu/ec/ConstruccionAPI/ConstruccionApiApplicationTests.java
@@ -0,0 +1,13 @@
+package epn.edu.ec.ConstruccionAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConstruccionApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
